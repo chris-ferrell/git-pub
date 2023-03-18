cf593e32064b192b551fd1b680015ddd4de57cae
unknown-bytes <unknown.develops@gmail.com>
2023-03-18 17:07:40 -0400

    added drinks.js data

--- server.js
+++ server.js
@@ -2,3 +2,14 @@
 const express = require('express');
 const app = express();
 const port  = 3000
+
+app.get('/', (req,res) => {
+    // .send kinda like a console.log() to the routes page?? 
+    res.send("Welcome to Git Pub");
+});
+// having the app listen on the "port" 
+app.listen(port, () => {
+    // logging that the app is successfully listening to the set PORT
+    console.log(`Listening👂 on port:${port}`);
+});
+
