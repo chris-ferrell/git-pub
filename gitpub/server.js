bf517f7847c9b91185e076b0957589e9355b3ea4
unknown-bytes <unknown.develops@gmail.com>
2023-03-24 04:16:47 -0400

    list && first cptl added

--- gitpub/server.js
+++ gitpub/server.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const drinks = require('./models/drinks.js')
 
 app.get('/', (req,res) => {
     res.send('Welcome to the Gitpub App!');
 })
 app.get('/drinks', (req,res) => {
-    res.render('drinks_index.ejs');
+    res.render(
+        'drinks_index.ejs', {
+            allDrinks:drinks
+        }
+    );
 });
 
 app.listen(PORT, () => {
