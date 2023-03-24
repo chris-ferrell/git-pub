9ecaebd38663265751d6d6317ab6257ed4441293
unknown-bytes <unknown.develops@gmail.com>
2023-03-24 03:48:31 -0400

    /drinks working

--- gitpub/server.js
+++ gitpub/server.js
@@ -5,7 +5,9 @@ const PORT = 3000;
 app.get('/', (req,res) => {
     res.send('Welcome to the Gitpub App!');
 })
-
+app.get('/drinks', (req,res) => {
+    res.render('drinks_index.ejs');
+});
 
 app.listen(PORT, () => {
     console.log("listening.. ");
