e2db4c22360d11c57a575f64e9e4109dc71119ab
unknown-bytes <unknown.develops@gmail.com>
2023-03-24 13:04:34 -0400

    added /drinks/:id

--- gitpub/server.js
+++ gitpub/server.js
@@ -13,7 +13,9 @@ app.get('/drinks', (req,res) => {
         }
     );
 });
-
+app.get('/drinks/:id', (req,res) => {
+    res.send(req.params.id)
+})
 app.listen(PORT, () => {
     console.log("listening.. ");
 });
