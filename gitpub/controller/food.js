7454ba4c3086899705f8563b43d0fea966e43043
unknown-bytes <unknown.develops@gmail.com>
2023-03-25 16:18:54 -0400

    ...

--- gitpub/controller/food.js
+++ gitpub/controller/food.js
@@ -8,22 +8,21 @@ const router = express.Router();
 
 
 // register routes 
-router.get('/:id?', (req,res) => {
-    if(req.params.id === undefined) {
-        // food index goes here
-        res.render(
-            'food_index.ejs', {
-             allFoods:food,
-             oneFood:food
-        })
-    } else { 
-        // food show goes here
-        const index = req.params.id;
-        res.render(
-            'food_show.ejs',{
-                oneFood:food[index],
-                foodIndex:index
-            })
-    }
+router.get('/', (req,res) => {
+    res.render('food_index', {
+        allFoods:food
+    })
 })
+
+
+
+router.get('/:id', (req,res) => {
+    // console.log(req.params.id)
+    const index = req.params.id;
+    res.render('food_show.ejs', {
+        oneFood:food[index]
+    })
+    }) 
 module.exports = router;
+
+
--- gitpub/server.js
+++ gitpub/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const cors = require('cors');
+app.use(cors());
 // const drinks = require('./models/drinks.js')
 const drinksRouter = require('./controller/drinks') // import router
 const foodRouter = require('./controller/food')
